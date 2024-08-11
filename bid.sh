cc0cc84fb39c38e157fd551f2750ae8f92045d0e
thebidsbot <thebidsbot2005@gmail.com>
2024-08-11 17:31:37 +0100

    Update bid.sh

--- bid.sh
+++ bid.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-sleep 4
-
 sudo apt-get update && sudo apt get upgrade -y
 clear
 
